c35dbde725e2ad7e53d8195ddb6fe616b01b0810
sababot <twinsgardos@gmail.com>
2022-04-27 19:56:18 +0200

    new clean

---
+++ static/scripts/dune.js
@@ -0,0 +1,9 @@
+function expand() {
+	document.getElementById('dune').style.marginTop = '10vw';
+	document.getElementById('dune').style.height = '80vh';
+}
+
+function back() {
+	document.getElementById('dune').style.marginTop = '65vh';
+	document.getElementById('dune').style.height = '16.5vh';
+}
