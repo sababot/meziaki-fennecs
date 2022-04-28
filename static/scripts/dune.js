76355e8fe473b3c62bde2d3acc1d5daaaf2b788a
sababot <twinsgardos@gmail.com>
2022-04-28 23:07:41 +0200

    explore upgrade

--- static/scripts/dune.js
+++ static/scripts/dune.js
@@ -1,9 +1,21 @@
-function expand() {
-	document.getElementById('dune').style.marginTop = '10vw';
-	document.getElementById('dune').style.height = '80vh';
-}
+var expanded = false;
 
-function back() {
-	document.getElementById('dune').style.marginTop = '65vh';
-	document.getElementById('dune').style.height = '16.5vh';
+function expand() {
+	if (expanded == false) {
+		document.getElementById('dune').style.marginTop = '10vw';
+		document.getElementById('dune').style.height = '80vh';
+
+		document.getElementById('expand_button').style.transform = 'rotate(90deg)';
+
+		expanded = true;
+	}
+
+	else if (expanded == true) {
+		document.getElementById('dune').style.marginTop = '65vh';
+		document.getElementById('dune').style.height = '16.5vh';
+
+		document.getElementById('expand_button').style.transform = 'rotate(-90deg)';
+
+		expanded = false;
+	}
 }
