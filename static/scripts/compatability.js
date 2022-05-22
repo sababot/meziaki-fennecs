5db2a197e9a9fd851c267c0ee2336760a97eb798
sababot <twinsgardos@gmail.com>
2022-05-22 18:20:54 +0200

    mobile support home

--- static/scripts/compatability.js
+++ static/scripts/compatability.js
@@ -2,23 +2,44 @@ var desert = document.getElementById("desert-svg");
 
     if ((window.innerWidth / window.innerHeight) < 0.75) {
         desert.src = "../static/images/desertsvg_portrait.png";
+
         document.getElementById('sidebar-sleep').style.display = 'block';
+
         document.getElementById('header-sleep').style.display = 'none';
-        document.getElementById('dune').style.height = '40vh';
+
+        document.getElementById('rock').style.display = 'none';
+
+        document.getElementById('morph-text').style.transform = 'rotate(90deg) translate(0vw, 143.5%)';
+        document.getElementById('morph-text').style.width = '90vh';
+        document.getElementById('morph-text').style.marginTop = '15vh';
     }
 
     else if ((window.innerWidth / window.innerHeight) > 1.25){
         desert.src = "../static/images/desertsvg_landscape_compressed.webp";
+
         document.getElementById('sidebar-sleep').style.display = 'none';
+
         document.getElementById('header-sleep').style.display = 'block';
-        document.getElementById('dune').style.height = '10vh';
+
+        document.getElementById('rock').style.display = 'block';
+
+        document.getElementById('morph-text').style.transform = 'rotate(0deg) translate(-143.5%)';
+        document.getElementById('morph-text').style.width = '90vw';
+        document.getElementById('morph-text').style.marginTop = '0';
     }
 
     else if ((window.innerWidth / window.innerHeight) < 1.25){
         desert.src = "../static/images/desertsvg_portrait.png";
+
         document.getElementById('sidebar-sleep').style.display = 'block';
+
         document.getElementById('header-sleep').style.display = 'none';
-        document.getElementById('dune').style.height = '10vh';
+
+        document.getElementById('rock').style.display = 'none';
+
+        document.getElementById('morph-text').style.transform = 'rotate(0deg) translate(-143.5%)';
+        document.getElementById('morph-text').style.width = '90vw';
+        document.getElementById('morph-text').style.marginTop = '7.5vh';
     }
 
 window.addEventListener('resize', function(){
@@ -26,22 +47,43 @@ window.addEventListener('resize', function(){
 
     if ((window.innerWidth / window.innerHeight) < 0.75) {
         desert.src = "../static/images/desertsvg_portrait.png";
+
         document.getElementById('sidebar-sleep').style.display = 'block';
+
         document.getElementById('header-sleep').style.display = 'none';
-        document.getElementById('dune').style.height = '40vh';
+
+        document.getElementById('rock').style.display = 'none';
+
+        document.getElementById('morph-text').style.transform = 'rotate(90deg) translate(0vw, 143.5%)';
+        document.getElementById('morph-text').style.width = '90vh';
+        document.getElementById('morph-text').style.marginTop = '15vh';
     }
 
     else if ((window.innerWidth / window.innerHeight) > 1.25){
         desert.src = "../static/images/desertsvg_landscape_compressed.webp";
+
         document.getElementById('sidebar-sleep').style.display = 'none';
+
         document.getElementById('header-sleep').style.display = 'block';
-        document.getElementById('dune').style.height = '10vh';
+
+        document.getElementById('rock').style.display = 'block';
+
+        document.getElementById('morph-text').style.transform = 'rotate(0deg) translate(-50%)';
+        document.getElementById('morph-text').style.width = '90vw';
+        document.getElementById('morph-text').style.marginTop = '0';
     }
 
     else if ((window.innerWidth / window.innerHeight) < 1.25){
         desert.src = "../static/images/desertsvg_portrait.png";
+
         document.getElementById('sidebar-sleep').style.display = 'block';
+
         document.getElementById('header-sleep').style.display = 'none';
-        document.getElementById('dune').style.height = '10vh';
+
+        document.getElementById('rock').style.display = 'none';
+
+        document.getElementById('morph-text').style.transform = 'rotate(0deg) translate(-50%)';
+        document.getElementById('morph-text').style.width = '90vw';
+        document.getElementById('morph-text').style.marginTop = '7.5vh';
     }
 });
