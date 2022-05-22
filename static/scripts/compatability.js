b68a247f3e4c0dff30e03e50a4a400398e15493d
sababot <twinsgardos@gmail.com>
2022-05-22 12:08:43 +0200

    sizing

--- static/scripts/compatability.js
+++ static/scripts/compatability.js
@@ -1,30 +1,47 @@
-// Init
 var desert = document.getElementById("desert-svg");
 
-if ((window.innerWidth / window.innerHeight) > 1.25){
-    desert.src = "../static/images/desertsvg_landscape_compressed.webp";
-    document.getElementById('sidebar-sleep').style.display = 'none';
-    document.getElementById('header-sleep').style.display = 'block';
-}
+    if ((window.innerWidth / window.innerHeight) < 0.75) {
+        desert.src = "../static/images/desertsvg_portrait.png";
+        document.getElementById('sidebar-sleep').style.display = 'block';
+        document.getElementById('header-sleep').style.display = 'none';
+        document.getElementById('dune').style.height = '40vh';
+    }
 
-else if ((window.innerWidth / window.innerHeight) < 1.25){
-    desert.src = "../static/images/desertsvg_portrait.png";
-    document.getElementById('sidebar-sleep').style.display = 'block';
-    document.getElementById('header-sleep').style.display = 'none';
-}
-
-window.addEventListener('resize', function(){
-    var desert = document.getElementById("desert-svg");
-
-    if ((window.innerWidth / window.innerHeight) > 1.25){
+    else if ((window.innerWidth / window.innerHeight) > 1.25){
         desert.src = "../static/images/desertsvg_landscape_compressed.webp";
         document.getElementById('sidebar-sleep').style.display = 'none';
         document.getElementById('header-sleep').style.display = 'block';
+        document.getElementById('dune').style.height = '10vh';
     }
 
     else if ((window.innerWidth / window.innerHeight) < 1.25){
         desert.src = "../static/images/desertsvg_portrait.png";
         document.getElementById('sidebar-sleep').style.display = 'block';
         document.getElementById('header-sleep').style.display = 'none';
+        document.getElementById('dune').style.height = '10vh';
+    }
+
+window.addEventListener('resize', function(){
+    var desert = document.getElementById("desert-svg");
+
+    if ((window.innerWidth / window.innerHeight) < 0.75) {
+        desert.src = "../static/images/desertsvg_portrait.png";
+        document.getElementById('sidebar-sleep').style.display = 'block';
+        document.getElementById('header-sleep').style.display = 'none';
+        document.getElementById('dune').style.height = '40vh';
+    }
+
+    else if ((window.innerWidth / window.innerHeight) > 1.25){
+        desert.src = "../static/images/desertsvg_landscape_compressed.webp";
+        document.getElementById('sidebar-sleep').style.display = 'none';
+        document.getElementById('header-sleep').style.display = 'block';
+        document.getElementById('dune').style.height = '10vh';
+    }
+
+    else if ((window.innerWidth / window.innerHeight) < 1.25){
+        desert.src = "../static/images/desertsvg_portrait.png";
+        document.getElementById('sidebar-sleep').style.display = 'block';
+        document.getElementById('header-sleep').style.display = 'none';
+        document.getElementById('dune').style.height = '10vh';
     }
 });
