f60aaf3c1ed19ec7b4e39dcff95e218ad93fd062
sababot <twinsgardos@gmail.com>
2022-06-12 19:30:20 +0200

    general clean

--- static/scripts/loader.js
+++ static/scripts/loader.js
@@ -1,7 +1,3 @@
-function func() {
-	var myVar = setTimeout(showPage, 2500);
-}
-
 function showPage() {
 	document.getElementById("loader").style.opacity = 0;
 	document.getElementById("loader_div").style.opacity = 0;
