25e31531e1e4272a19416cbe0bf4398651ae35f2
sababot <twinsgardos@gmail.com>
2022-06-16 20:00:37 +0200

    manifesto section refurbished

--- static/scripts/manifesto.js
+++ static/scripts/manifesto.js
@@ -1,16 +1,16 @@
 if ((window.innerWidth / window.innerHeight) > 1.25){
-        document.getElementById('manifesto').style.display = 'block';
+        document.getElementById('manifesto').style.display = 'flex';
         document.getElementById('manifesto-text-mobile').style.display = 'none';
     }
 
     else if ((window.innerWidth / window.innerHeight) < 1){
         document.getElementById('manifesto').style.display = 'none';
-        document.getElementById('manifesto-mobile').style.display = 'block';
+        document.getElementById('manifesto-text-mobile').style.display = 'block';
     }
 
 window.addEventListener('resize', function(){
     if ((window.innerWidth / window.innerHeight) > 1.25){
-        document.getElementById('manifesto').style.display = 'block';
+        document.getElementById('manifesto').style.display = 'flex';
         document.getElementById('manifesto-text-mobile').style.display = 'none';
     }
 
--- static/scripts/sidebar2.js
+++ static/scripts/sidebar2.js
@@ -1,14 +1,22 @@
 var sidebar = document.getElementById('sidebar');
 var show = false;
 
+function disableOpacity() {
+	sidebar.style.opacity = 0;
+	sidebar.style.zIndex = -1;
+}
+
 function showSidebar() {
 	sidebar.style.transition = 'transform 0.5s';
 	sidebar.style.transform = 'translate(0vw)';
+	sidebar.style.opacity = 1;
+	sidebar.style.zIndex = 15;
 	show = true;
 }
 
 function hideSidebar() {
 	setTimeout(disableTransition, 500);
+	setTimeout(disableOpacity, 500);
 	sidebar.style.transform = 'translate(100vw)';
 	show = false;
 }
