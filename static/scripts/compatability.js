29ec9f828c988d4518500c354527c9111fc70566
sababot <twinsgardos@gmail.com>
2022-07-06 23:03:05 +0200

    fennecs pop up

--- static/scripts/compatability.js
+++ static/scripts/compatability.js
@@ -1,6 +1,5 @@
 var desert = document.getElementById("desert-svg");
 
-var desert = document.getElementById("desert-svg");
 
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
         document.getElementById('container').style.display = 'none';
@@ -70,7 +69,7 @@ window.addEventListener('resize', function(){
     else if ((window.innerWidth / window.innerHeight) > 1.25){
         desert.src = "../static/images/desertsvg_landscape_compressed.webp";
 
-        document.getElementById('rock').style.display = 'block';
+        //document.getElementById('rock').style.display = 'block';
 
         //document.getElementById('morph-text').style.transform = 'rotate(0deg) translate(-50%)';
         //document.getElementById('morph-text').style.width = '90vw';
@@ -83,7 +82,7 @@ window.addEventListener('resize', function(){
     else if ((window.innerWidth / window.innerHeight) < 1.25){
         desert.src = "../static/images/desertsvg_portrait.png";
 
-        document.getElementById('rock').style.display = 'none';
+        //document.getElementById('rock').style.display = 'none';
 
         //document.getElementById('morph-text').style.transform = 'rotate(0deg) translate(-50%)';
         //document.getElementById('morph-text').style.width = '90vw';
--- static/scripts/dune.js
+++ static/scripts/dune.js
@@ -4,7 +4,7 @@ var show = false;
 function showall() {
 	show = true;
 
-	for (var i = 1; i < 4; i++) {
+	for (var i = 1; i < 9; i++) {
 		document.getElementById('fennec_' + i).style.transform = "translate(0%, -99%)";
 	}
 }
@@ -12,21 +12,21 @@ function showall() {
 function hideall() {
 	show = false;
 
-	for (var i = 1; i < 4; i++) {
+	for (var i = 1; i < 9; i++) {
 		document.getElementById('fennec_' + i).style.transform = "translate(0%, 0%)";
 	}
 }
 
 function fennecs() {
 	if (show == false) {
-		let rand = Math.floor(Math.random() * 4) + 1;
+		let rand = Math.floor(Math.random() * 9) + 1;
 
 		while (prev == rand) {
-			rand = Math.floor(Math.random() * 4) + 1;
+			rand = Math.floor(Math.random() * 9) + 1;
 		}
 
-		if (rand != 4) {
-			for (var i = 1; i < 4; i++) {
+		if (rand != 9) {
+			for (var i = 1; i < 9; i++) {
 				if (i != rand) {
 					document.getElementById('fennec_' + i).style.transform = "translate(0%, 0%)";
 				}
@@ -38,7 +38,7 @@ function fennecs() {
 		}
 
 		else {
-			for (var i = 1; i < 4; i++) {
+			for (var i = 1; i < 9; i++) {
 				document.getElementById('fennec_' + i).style.transform = "translate(0%, 0%)";
 			}
 		}
